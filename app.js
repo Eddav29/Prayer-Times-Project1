4620d6e8332bb6895eeee24eea3f18622fbc3cd9
Eddo Dava <eddodava@gmail.com>
2023-07-10 07:54:37 +0700

    membuat permission geoLocation

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+function success(position) {
+  console.log(position);
+}
+
+function error() {
+  alert("posisi tidak dapat diakses");
+}
+function userLocation() {
+  if (!navigator.geolocation) {
+    alert(
+      "GeoLocation tidak didukung didalam browser anda, silahkan gunakan browser lain"
+    );
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+}
 function index() {
   let app = document.getElementById("app");
   let h3 = document.createElement("h3");
   h3.innerHTML = "Prayer Times";
 
   app.appendChild(h3);
+
+  userLocation();
 }
 index();
